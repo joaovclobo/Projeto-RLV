ed6495d01ac32ca602ecb1f82bbd7c33832e164e
João Lobo <joao-lobo12@hotmail.com>
2024-03-15 14:58:24 -0300

    chore: Carrega os arquivos do projeto
    Este é o commit inicial após criado o repositório. Ele adiciona os arquivos padrões de um projeto Next.js com as seguintes configurações habilitadas: TyepScrip, ESLint, Tailwind CSS

---
+++ projeto-rlv/src/pages/temporarios/helloworld.tsx
@@ -0,0 +1,8 @@
+export default function PaginaPrincipal() {
+
+    return (
+        <div className="flex flex-col gap-5 justify-center items-center h-screen relative">
+            Hello Wold
+        </div>
+    )
+}
